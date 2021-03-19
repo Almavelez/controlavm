e3bc562953583154fde71636459e468ec6dc2959
Almavelez <74258817+Almavelez@users.noreply.github.com>
2021-03-19 08:20:45 -0600

    Create mi-footer.js

---
+++ cmp/mi-footer.js
@@ -0,0 +1,11 @@
+class MiFooter
+  extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = /* html */
+      `&copy; 2021 Alma velez 
+      Basado en pagina https://gilpgdm.github.io/6_iot/index.html de Gilberto 
+      Pacheco Gallegos.`;
+  }
+}
+customElements.define(
+  "mi-footer", MiFooter);
