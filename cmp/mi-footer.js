19c076f8cf5e1c9177873b79817f2ee7eda7dec8
Almavelez <74258817+Almavelez@users.noreply.github.com>
2021-03-25 17:02:52 -0600

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -3,45 +3,7 @@ class MiFooter
   connectedCallback() {
     this.innerHTML = /* html */
       
-    `<ul>
-    <li>
-      <a href="index.html">
-        <span class=
-            "material-icons">
-          home
-        </span>
-        Inicio
-      </a>
-    </li>
-    <li>
-      <a href="gps.html">
-        <span class=
-            "material-icons">
-          location_searching
-        </span>
-        GPS
-      </a>
-    </li>
-    <li>
-      <a href="archivos.html">
-        <span class=
-            "material-icons">
-          attach_file
-        </span>
-        Archivos
-      </a>
-    </li>
-    <li>
-      <a href="ayuda.html">
-        <span class=
-            "material-icons">
-          help
-        </span>
-        Ayuda
-      </a>
-    </li>
-  </ul>
-    <br>
+    `
     &copy; 2021 Alma velez 
       Basado en pagina https://gilpgdm.github.io/6_iot/index.html de Gilberto 
       Pacheco Gallegos.`;
