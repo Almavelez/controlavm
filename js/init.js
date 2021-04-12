15abc7a06d670ef54f2c72b2668c31c48b52a9da
almavelez <74258817+almavelez@users.noreply.github.com>
2021-04-12 15:31:23 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,11 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  apiKey: "AIzaSyDlPD5mqOXh18fjTIn7Z95dB3JHiKXqf6Y",
+  authDomain: "controlavmz.firebaseapp.com",
+   projectId: "controlavmz",
+  storageBucket: "controlavmz.appspot.com",
+  messagingSenderId: "340353702412",
+  appId: "1:340353702412:web:3e91d4140bdf5ca40dac29",
+  measurementId: "G-E7GBEMTN4Z"
 });
+  
